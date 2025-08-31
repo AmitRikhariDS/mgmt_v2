b90c6f35c71ceb3a53b56b85e9b390b4521c5686
Amit Rikhari <techyink011+123@gmail.com>
2025-09-01 00:23:04 +0530

    Notification included

--- field_mgmt/urls.py
+++ field_mgmt/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from dashboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', views.home_view, name='home'),
     path('', include('accounts.urls', namespace='accounts')),
     path("dashboard/", include(("dashboard.urls", "dashboard"), namespace="dashboard")),
 ]
